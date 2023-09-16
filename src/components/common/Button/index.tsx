042af9357225ee6b9e3c83b998df07aa3ac35ae0
huydao-agilityio <huy.dao@asnet.com>
2023-09-16 14:32:13 +0700

    feat: define the Button component and set styles to it

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,33 @@
+import { ButtonHTMLAttributes, MouseEventHandler, ReactNode } from 'react';
+
+// Styles
+import './button.css';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: string;
+  children?: ReactNode;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+const Button = ({ variant = '', children = '', onClick }: ButtonProps) => {
+  let buttonClassName = '';
+
+  switch (variant) {
+    case 'primary':
+      buttonClassName = 'button-primary';
+      break;
+    case 'secondary':
+      buttonClassName = 'button-secondary';
+      break;
+    default:
+      buttonClassName = 'button-tertiary';
+  }
+
+  return (
+    <button className={`button ${buttonClassName}`} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
