4ff4ce2130775bc49cdf4dbe459f54ca9a053eee
huydao-agilityio <huy.dao@asnet.com>
2023-09-16 15:47:43 +0700

    feat: add default props

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -9,21 +9,26 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const Input = ({ id, label, type, placeholder, onChange }: InputProps) => {
-  return (
-    <div className="form-control">
-      <label htmlFor={id} className="form-label">
-        {label}
-      </label>
-      <input
-        type={type}
-        placeholder={placeholder}
-        id={id}
-        className="form-enter"
-        onChange={onChange}
-      />
-    </div>
-  );
+const Input = ({ id, label, type, placeholder, onChange }: InputProps) => (
+  <div className="form-control">
+    <label htmlFor={id} className="form-label">
+      {label}
+    </label>
+    <input
+      type={type}
+      placeholder={placeholder}
+      id={id}
+      className="form-enter"
+      onChange={onChange}
+    />
+  </div>
+);
+
+Input.defaultProps = {
+  id: '',
+  label: '',
+  type: '',
+  onchange: () => {}
 };
 
 export default Input;
